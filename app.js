be34af2a057bd7dddad56d00a77e1f3c1508c35f
Aiuner <demonspawndaemnyxea@gmail.com>
2020-07-14 16:52:00 -0400

    Added in/updated a lot of comments to the JS.

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+//
+//global variables are all set up here
+//
 const domain = `https://pokeapi.co/api/v2/`
 let type1 = null;
 let type2 = null;
@@ -7,34 +10,38 @@ let typeAns1 = null;
 let typeAns2 = null;
 let roundAnswers = [];
 const indexLimiter = 801;
+let rtPanel = document.getElementById("right-answers");
 
-//this is fine and doesn't need tweaking
-let randomIndex = () => { //function to generate a pokemon randomly based on its index number
+// This function generates a random index number for a Pokemon
+//
+let randomIndex = () => {
     let randomIndexNo = Math.ceil(Math.random()*indexLimiter);
-    //console.log(randomIndexNo);
     return randomIndexNo;
 }
 
-let fetchData = async (searchCriteria, saveTypes = true) => { //fetches data for the pokemon at the index number supplies. saveTypes prevents it from overiding the type answers when generating the random result.
+// This is a combined function for getting Pokemon names either from their index number or from a list of Pokemon by their type from PokeAPI.
+//
+let fetchData = async (searchCriteria, saveTypes = true) => { //saveTypes is used to tell the function whether or not we're generating information for the answer Pokemon or for the random answer.
+
     if (isNaN(searchCriteria)) {
+        //searchCriteria is a type, this api call returns a list of Pokemon of that type.
         const namesByType = `${domain}type/${searchCriteria}`;
         try {
             let pkmnNames = await axios.get(namesByType);
 
-            //namesByType as an array, checks URL of each and toss any where ${domain}pokemon/(index#) index# is > 151
-
-            for (let i = pkmnNames.data.pokemon.length-1; i >= 0; i--) {
-
-                let currentID = convertURLtoIndex(pkmnNames.data.pokemon[i].pokemon.url);
-                if (currentID > indexLimiter) {
-                    pkmnNames.data.pokemon.pop();
+            //if the game's indexLimiter is set to use a value less than PokeAPI's current maximum Pokedex ID value, it will run the for loop to remove any Pokemon in the type lists whose ID exceed that value.
+            if (indexLimiter < 801) {
+                for (let i = pkmnNames.data.pokemon.length-1; i >= 0; i--) {
+                    let currentID = convertURLtoIndex(pkmnNames.data.pokemon[i].pokemon.url);
+                    if (currentID > indexLimiter) {
+                        pkmnNames.data.pokemon.pop();
+                    }
                 }
             }
-            //start from end of array and pop until all >151 are gone
-    
+            //selects a random Pokemon from list for the type its currently looking at.
             let ansByType = Math.ceil(Math.random()*(pkmnNames.data.pokemon.length-1));
 
-            console.log(pkmnNames.data.pokemon[ansByType].pokemon.name);
+            //returns the name of the randomly selected pokemon from the current list
             return pkmnNames.data.pokemon[ansByType].pokemon.name;
         }
         catch (error) {
@@ -42,14 +49,16 @@ let fetchData = async (searchCriteria, saveTypes = true) => { //fetches data for
         }
     }
     else {
+        //searchCriteria is an index number, this API call returns data for the Pokemon with that index number.
         const pkmnByIndexNo = `${domain}pokemon/${searchCriteria}`;
         try {
             let whoisPkmn = await axios.get(pkmnByIndexNo);
-            console.log(whoisPkmn.data.name);
 
+            //if saveTypes is true, this is the answer Pokemon and its types are needed to generate two of the answers.
             if (saveTypes) {
-                genSilhouette(searchCriteria.toString());
+                genSilhouette(searchCriteria.toString()); //gets the silhouette to be displayed for the game
 
+                //the if statement deals with the case where a Pokemon is a monotype. If it is, both random Pokemon chosen based on its type will be from the same type list.
                 type1 = whoisPkmn.data.types[0].type.name;
                 if (whoisPkmn.data.types.length > 1) {
                     type2 = whoisPkmn.data.types[1].type.name;
@@ -57,15 +66,18 @@ let fetchData = async (searchCriteria, saveTypes = true) => { //fetches data for
                 else {
                     type2 = whoisPkmn.data.types[0].type.name;
                 }
-                //console.log(`${type1} ${type2}`);
 
+                //checks to make sure type2 was determined, which is done after type1. If type2 is null, the answers can't be fetched.
+                //these are both "await" because they need to wait for the recursive call to fetchData to return Pokemon names from the type lists.
                 if (type2 !== null) {
                     typeAns1 = await fetchData(type1);
                     typeAns2 = await fetchData(type2);
                 }
+                //adds the two type-based answers to the possible answer choices for the round. They are stored in an array.
                 roundAnswers.push(typeAns1);
                 roundAnswers.push(typeAns2);
             }
+            //returns the name of the Pokemon for the index number that had been passed into this part of the fetchData function.
             return whoisPkmn.data.name;
         }
         catch (error) {
@@ -74,42 +86,59 @@ let fetchData = async (searchCriteria, saveTypes = true) => { //fetches data for
     }
 }
 
-//game starts
-
+//clicking on the yes-buttom ("Let's Play!") begins the game by calling the first game function, nextRound().
+//
 let startButton = document.querySelector("#yes-button");
 startButton.addEventListener('click', async (e) => {
+    nextRound();
+});
+
+//the nextRound function, which starts the next round of the game when it's called. Takes care of clearing out the old answers, updating the pokemon silhouette and adding new answers, etc.
+//
+let nextRound = async () => {
+    roundAnswers.splice(0, roundAnswers.length)
     clearRtPanel();
     correctAns = await fetchData(randomIndex()); //gets silhouette pokemon
     randomAns = await fetchData(randomIndex(), false); //gets random pokemon answer
     roundAnswers.push(correctAns);
     roundAnswers.push(randomAns);
     answerButtons(roundAnswers);
-});
+}
 
+//used to get back the index numbers for Pokemon listed by type. The API does not return the index numbers when used this way, but does return the Pokemon's URL, which *does* have its index number at the very end. Since the Pokemon urls always have the same format, they can be split into an array with the last value being the index number. This is only used when indexLimiter is less than 801.
+//
 let convertURLtoIndex = (url) => {
     let pokeURL = url;
-    //console.log(pokeURL);
     let urlArr = pokeURL.split(`/`);
-    //console.log(urlArr);
+
     return urlArr[6];
 }
 
+//PokeAPI doesn't have the official art for the Pokemon, only their sprites (which are very tiny.) Serebii.net has the official art and the urls are all in the same format. This function appends zeroes to the front of the index numbers and takes the last 3 digits to ensure that serebii's URL doesn't 404 due to using 3-digit Pokedex index numbers. Once it has the full url, it replaces the current silhouette image with the new one.
+//
 let genSilhouette = (index) => {
     let indexNo = index;
-    //console.log(indexNo);
-    indexNo = ("00" + indexNo).slice(-3) //appends two zeroes in front, then takes the last three digits- adds placehodler zeroes for serebii url to work.
+    indexNo = ("00" + indexNo).slice(-3)
     document.querySelector('.silhouette').src = `https://serebii.net/pokemon/art/${indexNo}.png`
 }
 
-let rtPanel = document.getElementById("right-answers");
-
+// simple function deletes all the previous contents of a panel, used at the start of a round to clear the previous round's contents a la nextRound()
+//
 let clearRtPanel = () => {
     while (rtPanel.firstChild) {
         rtPanel.removeChild(rtPanel.firstChild);
     }
 }
 
-//might need to ask for help with this
+// checks the text on an answer button to see if it matches the text stored in the correctAns variable.
+//
+let compareAnswer = (answer) => {
+    return (answer.innerHTML === correctAns) ? true : false;
+    //returns "true" is the text matches, returns "false" if it doesn't. 
+}
+
+//generates functioning answer buttons by taking in an array containing each possible answer, these are made AFTER the array of answers has been filled
+//
 let answerButtons = (rndAnswers) => {
     let answerList = document.createElement('div');
     answerList.classList.add("answerList");
@@ -119,30 +148,22 @@ let answerButtons = (rndAnswers) => {
         let answerButton = document.createElement('button');
         answerButton.classList.add("answers");
         answerButton.innerHTML = `${rndAnswers[i]}`
+        answerButton.addEventListener('click', () => { //adds click event to check answers
+            compareAnswer(answerButton); //calls compare function when button is clicked
+        })
         answerList.append(answerButton);
     }
-
 }
 
-// **generates a random index number to throw into url.
-// **fetches pokemon data based on index number - name = correct answer
-// **gets type information for that particular pokemon 
-// whoisPkmn is the correct answer
-
-// **uses types from that pokemon and throws them into a new url
-// **generate two more answers from type list
-
-// **generates a random index number to throw into url.
-// **fetches name of pokemon with that index number = last answer
 
 // randomize order of the answers
 // compare strings of answer with correct answer, if it's a match, the selected answer is correct
 
 
 
-// start game overides content from right panel ("would you like to play?" & button) with 4 possible answers (buttons)
-// replaces left panel image with random pokemon image
-// clicking an answer checks current guesses
+// **start game overides content from right panel ("would you like to play?" & button) with 4 possible answers (buttons)
+// **replaces left panel image with random pokemon image
+// **clicking an answer checks current guesses
 
 // set score = 0; before entering round loops
 // guess = 0;
